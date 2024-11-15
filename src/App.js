701fcf6c389d745c10f6345b5014d0b91b124448
Fu Lam <fulam@Fus-MacBook-Pro.local>
2024-11-16 00:09:57 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <h2 className="text-3xl font-bold underline">Home Page</h2>;
+};
+
+export default Home;
